d30d221bbbcfc96ed0cba91014c0d442f6c0e32a
reisenbest <drgn96rd@gmail.com>
2024-03-13 16:01:40 +0300

    Update settings.py

--- stakewolle_api_service/stakewolle_api_service/settings.py
+++ stakewolle_api_service/stakewolle_api_service/settings.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-   ),
+   ), #закомментировать для тестов без токенов напрямую
 
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
